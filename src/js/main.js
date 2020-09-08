f52476863f39d43b7b2ae3299a874704de652c42
Kartash_Dmitry <kartaschdim@gmail.com>
2020-09-08 17:54:40 +0300

    finish header footer tovar

--- src/js/main.js
+++ src/js/main.js
@@ -47,7 +47,6 @@ if (document.querySelector('.custom-select')){
             customOption.addEventListener('click', (event) => {              // Собитие для передачи выбранного опшена в основной селект
                 elem.selected = true
                 customSelected.innerText = customOption.innerText
-                console.log(customSelectField.querySelector('select'));
                 customSelect.classList.remove('active')
                 customOptions.classList.remove('active')
             })
@@ -62,19 +61,19 @@ if (document.querySelector('.custom-select')){
 /*  tovar counter btns      */
 
 if (document.querySelector('.qty')){
-    let qty = document.querySelector('.qty'),
-        counter = qty.querySelector('input')
-    qty.querySelector('.plus').addEventListener('click', (event)=>{
-        event.preventDefault();
-        counter.value++
+    document.querySelectorAll('.qty').forEach( qty => {
+        let counter = qty.querySelector('input')
+        qty.querySelector('.plus').addEventListener('click', (event)=>{
+            event.preventDefault();
+            counter.value++
+        })
+        qty.querySelector('.minus').addEventListener('click', (event)=>{
+            event.preventDefault();
+            if (counter.value > 1 ) {
+                counter.value--
+            }
+        })
     })
-    qty.querySelector('.minus').addEventListener('click', (event)=>{
-        event.preventDefault();
-        if (counter.value > 1 ) {
-            counter.value--
-        }
-    })
-
 }
 
 /*  tovar counter btns      */
@@ -84,7 +83,6 @@ if (document.querySelector('.qty')){
 if (document.querySelector('.tabs')){
     document.querySelectorAll('.tabs').forEach( tabs => {
         tabs.querySelectorAll('.links li').forEach( (elem, index) => {
-            console.log(elem)
             elem.addEventListener('click', () => {
                 tabs.querySelectorAll('.links li').forEach( el => {
                     el.classList.remove('current')
@@ -100,3 +98,121 @@ if (document.querySelector('.tabs')){
 }
 
 /*  tab  switcher       */
+
+/*  tovar calendar events   */
+
+if (document.querySelector('.events-calendar')){
+    let allDisabledEL = [];
+    document.querySelectorAll('.calendar_raids .raids_raid>div').forEach( el => {
+        if (el.classList.contains('disabled')){
+            allDisabledEL.push(el)
+        }
+    })
+    let disableCalendarTimes = function(){
+        let currentSelectedFraction = document.forms.eventsCalendar.elements.fraction.value,
+            anotherFraction = currentSelectedFraction === 'horde' ? 'alliance' : 'horde',
+            width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        if (width > 576) {
+            document.querySelectorAll(`.calendar_raids .raids_${anotherFraction}>div`).forEach(el => {
+                el.classList.remove('active')
+                el.classList.add('disabled')
+                allDisabledEL.forEach(disabledEl => {
+                    disabledEl.classList.add('disabled')
+                })
+            })
+            document.querySelectorAll(`.calendar_raids .raids_${currentSelectedFraction}>div`).forEach(el => {
+                el.classList.remove('active')
+                el.classList.remove('disabled')
+                allDisabledEL.forEach(disabledEl => {
+                    disabledEl.classList.add('disabled')
+                })
+            })
+        } else {
+            document.querySelector(`.calendar_raids .raids_${anotherFraction}`).style.display = 'none'
+            document.querySelector(`.calendar_raids .raids_${currentSelectedFraction}`).style.display = 'grid'
+        }
+    }
+    disableCalendarTimes()
+    document.querySelectorAll('.events-calendar .order-form_fraction label').forEach( elem => {
+        elem.addEventListener('click', disableCalendarTimes)
+    })
+    document.querySelector('.events-calendar .calendar_raids').addEventListener('click', function (event){
+        if (event.target.closest('.raids_time')){
+            if (document.querySelector('.events-calendar .raids_time.active')) {
+                document.querySelector('.events-calendar .raids_time.active').classList.remove('active')
+            }
+            let elem = event.target;
+            for ( ; elem && elem !== document; elem = elem.parentNode ) {
+                if (elem.classList.contains('raids_time')){
+                    if (!elem.classList.contains('disabled')){
+                        elem.classList.add('active')
+                    }
+                    return
+                }
+            }
+
+        }
+    })
+}
+
+/*  tovar calendar events   */
+
+/*  header search btn switch    */
+
+document.querySelector('.search_button').addEventListener('click', function (){
+    document.querySelector('.user-menu_search').classList.toggle('active')
+})
+
+/*  header search btn switch    */
+
+/* header mobile-menu switch    */
+
+document.querySelector('.user-menu_mobile-switch').addEventListener('click', function(){
+    this.classList.toggle('active')
+    document.querySelector('.header_mobile-menu').classList.toggle('active')
+    document.querySelector('.aside').classList.remove('active')
+    let mobileMenuCloseFunc = function (event){
+        if (!event.target.closest('.header_mobile-menu') && !event.target.closest('.user-menu_mobile-switch')) {
+            document.querySelector('.user-menu_mobile-switch').classList.remove('active')
+            document.querySelector('.header_mobile-menu').classList.remove('active')
+            document.removeEventListener('click', mobileMenuCloseFunc)
+        }
+    }
+    setTimeout( () => {
+        document.addEventListener('click', mobileMenuCloseFunc)
+    })
+})
+
+/* header mobile-menu switch    */
+
+document.addEventListener('scroll', function (){
+    if (pageYOffset > 0){
+        document.querySelector('main').style.marginTop = '55px'
+        document.querySelector('header').classList.add('fixed')
+        document.querySelector('.aside_wrapper').style.paddingTop = '55px'
+    } else {
+        document.querySelector('main').style.marginTop = '0'
+        document.querySelector('header').classList.remove('fixed')
+        document.querySelector('.aside_wrapper').style.paddingTop = '0'
+    }
+})
+
+document.querySelector('.header_mobile-sidebar-switcher').addEventListener('click', function(){
+    document.querySelector('.aside').classList.toggle('active')
+    document.querySelector('.user-menu_mobile-switch').classList.remove('active')
+    document.querySelector('.header_mobile-menu').classList.remove('active')
+    let asideCloseFunc = function (event){
+        if (!event.target.closest('.aside') && !event.target.closest('.header_mobile-sidebar-switcher')) {
+            document.querySelector('.aside').classList.remove('active')
+            document.removeEventListener('click', asideCloseFunc)
+        }
+    }
+    setTimeout( () => {
+        document.addEventListener('click', asideCloseFunc)
+    })
+})
+
+document.querySelector('.user-menu_color-theme').addEventListener('click', function (){
+    this.classList.toggle('active')
+    document.body.classList.toggle('dark-theme')
+})
